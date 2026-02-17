10e41950049104dc60616f974b09b4080cff922d
grantn5 <grant@cosyne.com>
2026-02-17 18:45:28 +0000

    update index page

--- render_cv.r
+++ render_cv.r
@@ -47,9 +47,3 @@ pagedown::chrome_print(
   input = tmp_html_cv_loc,
   output = here::here("docs/cv_engineer.pdf")
 )
-
-# Render landing page
-rmarkdown::render(
-  here::here("landing_page.rmd"),
-  output_file = here::here("docs/index.html")
-)
